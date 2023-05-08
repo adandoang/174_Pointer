14a37ba5c34e7bb5741cde867806bd937b6e7f03
adandoang <akhdanjzif@gmail.com>
2023-05-08 15:57:39 +0700

    deklarasi method

---
+++ 174_FriendClass_III/174_FriendClass_III.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class pelajar;
+class manusia {
+public:
+	void showNilaiPelajar(pelajar&);
+};
+class pelajar {
+private:
+	int nilai;
+public:
+	pelajar() {
+		nilai = 100;
+	}
+	friend void manusia::showNilaiPelajar(pelajar& x);
+};
+
